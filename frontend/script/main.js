8b6b51da40da73b404bec2a8e6b119e161d7d525
PhoengZ <pholapcondo11@gmail.com>
2025-01-03 21:22:28 +0700

    fix dropdown's animation

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -4,14 +4,13 @@ let link = document.querySelectorAll(".dropdown-content a");
 function navbar_show(){
     let check = window.getComputedStyle(nav_bar);
     let check_1 = window.getComputedStyle(dropdown);
-    let check_2 = window.getComputedStyle(link[0]);
     nav_bar.style.height = check.height == "0px" ?"150px":'0px';
     dropdown.style.backgroundColor = check_1.backgroundColor == "rgb(255, 255, 255)" ? "rgb(186, 186, 186)":"rgb(255, 255, 255)";
-    for (let i = 0;i<link.length;i++){
-        if (window.getComputedStyle(link[i]).visibility == "hidden"){
-            link[i].style.visibility = "visible";
-        }else{
-            link[i].style.visibility = "hidden";
+    for (let i = 0; i < link.length; i++) {
+        if (window.getComputedStyle(link[i]).opacity == "0" || getComputedStyle(link[i]).opacity === "0") {
+            link[i].style.opacity = "1";
+        } else {
+            link[i].style.opacity = "0";
         }
     }
 }
