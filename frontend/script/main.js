5a89f9771f7578591fc4eab10cb97741a8845279
PhoengZ <pholapcondo11@gmail.com>
2025-01-04 09:21:04 +0700

    Update dropdown transition

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -4,7 +4,7 @@ let link = document.querySelectorAll(".dropdown-content a");
 function navbar_show(){
     let check = window.getComputedStyle(nav_bar);
     let check_1 = window.getComputedStyle(dropdown);
-    nav_bar.style.height = check.height == "0px" ?"150px":'0px';
+    nav_bar.style.height = check.height == "0px" ?"170px":'0px';
     dropdown.style.backgroundColor = check_1.backgroundColor == "rgb(255, 255, 255)" ? "rgb(186, 186, 186)":"rgb(255, 255, 255)";
     for (let i = 0; i < link.length; i++) {
         if (window.getComputedStyle(link[i]).opacity == "0" || getComputedStyle(link[i]).opacity === "0") {
